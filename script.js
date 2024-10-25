09e34c6bd45d59ca5947b95dbe41ce3602ef7e46
Haroon Khan <hronkkhan6@gmail.com>
2024-10-25 13:47:31 -0700

    Module 4 code

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 
-// Setting up for loop to start at 1 and eend at 10
-for (let i = 1; i <= 10; i++) {
-    // If i/2 has a remainder of 0, then it is even. Otherwise, it is odd
-    if (i % 2 == 0) {
-        document.write(`Count ${i} is even. `)
+
+const validAmount = (amount) => {
+    if (amount >= 5 && amount <= 500) {
+        return true
     } else {
-        document.write(`Count ${i} is odd. `)
+        alert("Invalid Input")
+        return false
     }
 }
 
-
-// Set the variable to what the user will input
-let myNum = prompt("Pick a number between 5 and 20")
-// Set the counter to start at 1
-let counter = 1
-
-// The loop will write 1, and while the counter is less than or equal to the user's number, it will keep writing
-// The loop will stop once the user's number is reached
-do {
-    document.write(`${counter}, `)
-    counter ++
-} while (counter <= myNum)
-
-// Setting array of string values
-let text = ""
-let array = ["Accounting", "Algebra", "Programming", "Art", "Data Analytics"]
-
-// For each value in the array, perform the function
-array.forEach(myFunction)
-
-// Function that will run for each array value
-function myFunction (value, index) {
-    text += index + ": " + value + "<br>"
+const validService = (quality) => {
+    if (quality == "great" || quality == "ok" || quality == "poor") {
+        return true
+    } else {
+        alert("Invalid Input")
+        return false
+    }
 }
 
-document.getElementById("forEach").innerHTML = text
+const calcTip = (amount, tip, quality) => {
+    tipAmount = tip * 100
+    alert(`Your service was $${amount}, your recommended tip is $${tipAmount}, based on the ${quality} rating you gave us.`)
+}
+
+let tip = 0
+let amount = prompt("How much was the service? ($5 - $500): ")
+if (validAmount(amount) == true) {
+    let quality = prompt("Quality of the service? (great, ok, or poor):")
+    validService(quality)
+    if (quality == "great") {
+        tip = 0.20
+    } else if (quality == "ok") {
+        tip = 0.15
+    } else if (quality == "poor") {
+        tip = 0.10
+    }
+
+    calcTip(amount, tip, quality)
+}
+
+
+
+
+
+
+
+
+
+
+
+
